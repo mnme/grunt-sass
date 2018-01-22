e0f678deaf81fd4a480e353b12b26d3a55f136d9
Nicolas Jeker <n.jeker@gmx.net>
2018-01-22 22:52:19 +0100

    Use Promises instead of eachAsync

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,25 +1,49 @@
 'use strict';
 var path = require('path');
-var eachAsync = require('each-async');
 var sass = require('node-sass');
 
 module.exports = function (grunt) {
 	var options;
+
+	const sassCompile = function (options) {
+		return new Promise(function (resolve, reject) {
+			sass.render(options, function (error, result) {
+				if (error) {
+					reject(error.formatted);
+				} else {
+					resolve(result);
+				}
+			});
+		});
+	};
+
+	const sassWrite = function (to, options) {
+		return sassCompile(options)
+			.then(function (result) {
+				grunt.file.write(to, result.css);
+				if (options.sourceMap) {
+					if (typeof options.sourceMap === 'string') {
+						grunt.file.write(options.sourceMap, result.map);
+					} else {
+						grunt.file.write(to + '.map', result.map);
+					}
+				}
+			});
+	};
+
 	grunt.verbose.writeln('\n' + sass.info + '\n');
 	grunt.registerMultiTask('sass', 'Compile Sass to CSS', function () {
 		options = this.options({
 			precision: 10
 		});
-		eachAsync(this.files, function (f, i, next) {
+		Promise.all(this.files.map(function (f) {
 			if (!f.dest) {
-				grunt.log.error('No destination file specified');
-				next();
-				return;
+				grunt.log.warn('No destination file specified');
+				return Promise.resolve();
 			}
 			var src = f.src.filter(function (filepath) {
 				if (!grunt.file.exists(filepath)) {
-					grunt.log.warn('Source file ' + filepath + ' not found.');
-					return false;
+					throw new Error('Source file ' + filepath + ' not found.');
 				}
 				if (path.basename(filepath)[0] === '_') {
 					return false;
@@ -27,32 +51,18 @@ module.exports = function (grunt) {
 				return true;
 			});
 			if (src.length === 0) {
-				grunt.log.error('No source files were found.');
-				next();
-				return;
+				grunt.log.warn('No source files were found.');
+				return Promise.resolve();
 			}
 
-			src.map(function (srcpath) {
-				return sass.render(Object.assign({}, options, {
+			return Promise.all(src.map(function (srcpath) {
+				return sassWrite(f.dest, Object.assign({}, options, {
 					file: srcpath,
 					outFile: f.dest
-				}), function (err, res) {
-					if (err) {
-						grunt.log.error(err.formatted + '\n');
-						grunt.warn('');
-						next(err);
-						return;
-					}
-
-					grunt.file.write(f.dest, res.css);
-
-					if (options.sourceMap) {
-						grunt.file.write(this.options.sourceMap, res.map);
-					}
-
-					next();
-				});
-			});
-		}, this.async());
+				}));
+			}));
+		})).catch(function (err) {
+			grunt.log.error(err.formatted + '\n');
+		});
 	});
 };
