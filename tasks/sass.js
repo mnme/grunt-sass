c2f048ef943841f46119661fd74918e774c20604
Nicolas Jeker <n.jeker@gmx.net>
2018-01-22 20:49:54 +0100

    Make this work with file globbing patterns

--- tasks/sass.js
+++ tasks/sass.js
@@ -5,40 +5,55 @@ var assign = require('object-assign');
 var sass = require('node-sass');
 
 module.exports = function (grunt) {
+	var options;
 	grunt.verbose.writeln('\n' + sass.info + '\n');
-
 	grunt.registerMultiTask('sass', 'Compile Sass to CSS', function () {
-		eachAsync(this.files, function (el, i, next) {
-			var opts = this.options({
-				precision: 10
+		options = this.options({
+			precision: 10
+		});
+		eachAsync(this.files, function (f, i, next) {
+			if (!f.dest) {
+				grunt.log.error('No destination file specified');
+				next();
+				return;
+			}
+			var src = f.src.filter(function (filepath) {
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file ' + filepath + ' not found.');
+					return false;
+				}
+				if (path.basename(filepath)[0] === '_') {
+					return false;
+				}
+				return true;
 			});
-
-			var src = el.src[0];
-
-			if (!src || path.basename(src)[0] === '_') {
+			if (src.length === 0) {
+				grunt.log.error('No source files were found.');
 				next();
 				return;
 			}
 
-			sass.render(assign({}, opts, {
-				file: src,
-				outFile: el.dest
-			}), function (err, res) {
-				if (err) {
-					grunt.log.error(err.formatted + '\n');
-					grunt.warn('');
-					next(err);
-					return;
-				}
+			src.map(function (srcpath) {
+				return sass.render(assign({}, options, {
+					file: srcpath,
+					outFile: f.dest
+				}), function (err, res) {
+					if (err) {
+						grunt.log.error(err.formatted + '\n');
+						grunt.warn('');
+						next(err);
+						return;
+					}
 
-				grunt.file.write(el.dest, res.css);
+					grunt.file.write(f.dest, res.css);
 
-				if (opts.sourceMap) {
-					grunt.file.write(this.options.sourceMap, res.map);
-				}
+					if (options.sourceMap) {
+						grunt.file.write(this.options.sourceMap, res.map);
+					}
 
-				next();
+					next();
+				});
 			});
-		}.bind(this), this.async());
+		}, this.async());
 	});
 };
